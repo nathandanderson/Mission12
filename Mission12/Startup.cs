d6fe5d54bb15f85b0270b053f296f174e652dc90
Nathan Anderson <nathand.anderson@outlook.com>
2022-03-23 11:41:59 -0600

    Models and Database

--- Mission12/Startup.cs
+++ Mission12/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Mission12.Models;
 
 namespace Mission12
 {
@@ -24,6 +26,10 @@ namespace Mission12
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<TempleContext>(options =>
+            {
+                options.UseSqlite(Configuration["ConnectionStrings:TempleDBConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
